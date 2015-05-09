32b4e555afd9bc46c2bc6b9a4c989bac7eeeeaeb
samas92i <samuel.92i@hotmail.fr>
2015-05-09 22:00:25 +0200

    Fixe

---
+++ ssh/minecraft.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#Création par unixfox (Emilien)
+#Merci Julien008 pour les suggestions et report de bugs.
+
+#Options
+MC_PATH='' #Répertoire de votre serveur minecraft.
+NOM_JAR=''
+MEMALOC=512 #Mémoire à allouer à votre serveur minecraft.
+TPSWARN=10 #Temps après le quel le serveur va s'éteindre ou redémarrer.
+SCREEN_NAME=''
+
+#Variables
+server_stop() {
+        echo -n "Arrêt du serveur Minecraft..."
+        screen -S $SCREEN_NAME -p 0 -X stuff "`printf "say Arrêt du serveur dans $TPSWARN SECONDES.\r"`"
+        screen -S $SCREEN_NAME -p 0 -X stuff "`printf "save-all\r"`"
+        sleep ${TPSWARN}
+        screen -S $SCREEN_NAME -p 0 -X stuff "`printf "stop\r"`"
+        sleep 7
+        echo " [OK]"
+}
+
+server_start() {
+        echo -n "Lancement du serveur minecraft..."
+        cd $MC_PATH && screen -h 1024 -dmS $SCREEN_NAME java -jar -Xmx${MEMALOC}M -Xms512M -XX:MaxPermSize=128M -Dfile.encoding=UTF8 $NOM_JAR nogui; 
+        sleep 1
+        echo " [OK]"
+}
+
+commande() {
+     exec="$1";
+     echo "Exécution de la commande..."
+     screen -S $SCREEN_NAME -p 0 -X stuff "`printf "$exec\r"`"
+     sleep .1
+     echo " [OK]"
+}
+
+console() {
+     screen -r $SCREEN_NAME
+}
+
+# Corps du script
+case "$1" in
+  start)
+        server_start
+        ;;
+  stop)
+        server_stop
+        ;;
+  restart)
+    server_stop
+    server_start
+    ;;
+  exec)
+        if [ $# -gt 1 ]; then
+        shift
+        commande "$*"
+        else
+        echo "Vous devez spécifier une commande (exemple : 'help')."
+  fi
+  ;;
+  console)
+    console
+    ;;
+  *)
+        echo "Utilisation: service minecraft {start|stop|exec <commande>|console}"
+        exit 0
+        ;;
+esac
+
+exit 0
