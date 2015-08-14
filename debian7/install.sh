1c3a094f17f1dfecb30ba12e82a38b737afc245e
samas92i <samuel.92i@hotmail.fr>
2015-08-14 02:42:39 +0200

    add java & rename directory

--- debian7/install.sh
+++ debian7/install.sh
@@ -22,7 +22,7 @@ then
 	echo "Le panel est déjà installé !"
 else
 	# Installation du coeur
-	wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/filles/core.sh --no-check-certificate
+	wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/files/core.sh --no-check-certificate
 	chmod u+x core.sh
 	./core.sh
 
@@ -31,8 +31,18 @@ else
 	read -p "Installer SteamCMD ? (y/n): " steamcmd
 	if [[ "$steamcmd" == "y" || "$steamcmd" == "yes" || "$steamcmd" == "Y" ]]
     then
-		wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/filles/steamcmd.sh --no-check-certificate
+		wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/files/steamcmd.sh --no-check-certificate
 		chmod u+x steamcmd.sh
 		./steamcmd.sh
+    fi
+
+	# Installation de Java 7
+	echo ""
+	read -p "Installer Java 7 ? (y/n): " java
+	if [[ "$java" == "y" || "$java" == "yes" || "$java" == "Y" ]]
+    then
+		wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/files/java.sh --no-check-certificate
+		chmod u+x java.sh
+		./java.sh
     fi
 fi
