a4b05af21d07764f64285ba85da0ae0d3af39b62
samas92i <samuel.92i@hotmail.fr>
2015-08-14 02:06:47 +0200

    fix

--- debian7/install.sh
+++ debian7/install.sh
@@ -24,15 +24,15 @@ else
 	# Installation du coeur
 	wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/filles/core.sh --no-check-certificate
 	chmod u+x core.sh
-	sh core.sh
+	./core.sh
 
 	# Installation de SteamCMD
 	echo ""
 	read -p "Installer SteamCMD ? (y/n): " steamcmd
-	if [[ "$rootmysql" == "y" || "$rootmysql" == "yes" || "$rootmysql" == "Y" ]]
+	if [[ "$steamcmd" == "y" || "$steamcmd" == "yes" || "$steamcmd" == "Y" ]]
     then
 		wget https://raw.githubusercontent.com/samas92i/panelsks/master/debian7/filles/steamcmd.sh --no-check-certificate
 		chmod u+x steamcmd.sh
-		sh steamcmd.sh
+		./steamcmd.sh
     fi
 fi
